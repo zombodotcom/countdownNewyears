3bb24b211c5483b97430eb2ac9130d1ffff91ce1
MegapolisPlayer <megapolisplayer@protonmail.com>
2025-12-23 15:21:14 +0100

    bugfix: snow limit and interval calculation

--- src/routes/Snowflakes.svelte
+++ src/routes/Snowflakes.svelte
@@ -101,7 +101,7 @@
 				snowflakes.set(snowflakeId, o);
 				snowflakeId++;
 			},
-			HARD_SNOWFLAKE_LIMIT - calculatedSnowLimit + 25
+			((calculatedSnowLimit)/5) + 25
 		);
 
 		interval2 = setInterval(() => {
